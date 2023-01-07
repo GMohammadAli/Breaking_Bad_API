49c4321cdbfcb0a0f21d23dd7e0a5d582a4eb699
GMohammadAli <gulammohammadali@gmail.com>
2023-01-07 13:35:46 +0530

    Search functionality added

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import axios from "axios";
 import './App.css';
 import Header from './components/UI/Header';
 import CharacterGrid from './components/Characters/CharacterGrid';
+import Search from "./components/UI/Search";
+
 
 function App() {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
+    //The Breaking Bad API is not working for now, please visit: https://breakingbadapi.com/api to learn more
     const fetchItems = async () => {
-     const result = await axios(
-       `https://www.breakingbadapi.com/api/characters`
-     )
-     setItems(result.data)
-     setIsLoading(false)
-    }
+      const result = await axios(
+        `https://breakingbadapi.com/api/characters?name=${query}`
+      );
+      setItems(result.data);
+      setIsLoading(false);
+    };
 
-    fetchItems()
-  }, [])
+    fetchItems();
+  }, [query])
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery(q)}/>
       <CharacterGrid isLoading={isLoading} items={items}/>
     </div>
   );
--- src/components/Characters/CharacterGrid.js
+++ src/components/Characters/CharacterGrid.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Spinner from '../UI/Spinner';
 import CharacterItem from './CharacterItem';
 
 const CharacterGrid = ({isLoading, items}) => {
-  return isLoading ? (<h1>...Loading</h1>) : (<section className='cards'>{items.map(item => (
+  return isLoading ? (<h1><Spinner /></h1>) : (<section className='cards'>{items.map(item => (
     <CharacterItem key={item.char_id} item={item} />
   ))}</section >)
 }
